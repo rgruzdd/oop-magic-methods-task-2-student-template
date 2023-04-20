8d3c3eda0675fd1f9a0d993046009393c87acc8c
Gruzin <gruzd.roman@inbox.ru>
2023-04-20 19:36:14 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -1,14 +1,89 @@
 class Bird:
-    pass
+    def __init__(self, name):
+        self.name = name
 
+    def fly(self):
+        string = self.name + ' ' + 'bird can fly'
+        return string
+
+    def walk(self):
+        string = self.name + ' ' + 'bird can walk'
+        return string
+
+    def __str__(self):
+        string = str(self.name) + ' ' + 'bird can walk and fly'
+        return string
 
 class FlyingBird:
-    pass
+    def __init__(self, name, ration = 'grains'):
+        self.name = name
+        self.ration = ration
 
+    def fly(self):
+        string = self.name + ' ' + 'bird can fly'
+        return string
 
-class NonFlyingBird:
-    pass
+    def walk(self):
+        string = self.name + ' ' + 'bird can walk'
+        return string
 
+    def eat(self):
+        string = 'It eats mostly' + ' ' + self.ration
+
+        return string
+
+    def __str__(self):
+        string = self.name + ' ' + 'bird can walk and fly'
+        return string
+
+class NoFlyingBird:
+    def __init__(self, name, ration = 'fish'):
+        self.name = name
+        self.ration = ration
+
+    def walk(self):
+        string = self.name + ' ' + 'bird can walk'
+        return string
+
+    def eat(self):
+        string = 'It eats mostly' + ' ' + self.ration
+        return string
+
+    def swim(self):
+        string = self.name + ' ' + 'bird can swim'
+        return string
+
+    def __str__(self):
+        string = self.name + ' ' + 'bird can walk and swim'
+        return string
 
 class SuperBird:
-    pass
+    def __init__(self, name, ration = 1):
+        self.name = name
+
+    def fly(self):
+        string = self.name + ' ' + 'bird can fly'
+        return string
+
+    def walk(self):
+        string = self.name + ' ' + 'bird can walk'
+        return string
+
+    def eat(self):
+        string = 'It eats mostly fish'
+        return string
+
+    def swim(self):
+        string = self.name + ' ' + 'bird can swim'
+        return string
+
+    def __str__(self):
+        string = self.name + ' ' + 'bird can walk, swim and fly'
+        return string
+
+p = NoFlyingBird("Penguin")
+print(p.eat())
+
+
+
+
